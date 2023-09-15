2f11ce045091aedd3cbf700a33aada72ce5505c4
Abdul Qadir <aq452831@gmail.com>
2023-09-15 21:33:36 +0500

    class 3 updating

--- 03_class/index1.ts
+++ 03_class/index1.ts
@@ -2,20 +2,21 @@ let balance = 80;
 let minBalance= 85;
 let maxBalance=75;
 
-let result = balance + minBalance + maxBalance;
+let result = balance + minBalance + maxBalance; // You're creating a variable named "result" and calculating its value by adding "balance", "minBalance", and "maxBalance" together
 
-function creditScore(){
-    if(result>250 && result<=300){
-        return"A+";
+function creditScore(){ //This function will calculate a credit score based on the "result" value.
+    if(result>250 && result<=300){ //"result" is greater than 250 and less than or equal to 300.
+        return"A+";//If the condition in the previous if statement is true, the function returns the string "A+" as the credit score.
     }
-    else if(result>220 && result<=250){
-        return "B";
+    else if(result>220 && result<=250){ //result is greater than 220 and less than or equal to 250.
+        return "B";// If this condition is true, the function returns the string "B" as the credit score.
     }
-    else if(result>180 && result<=220){
+    else if(result>180 && result<=220){//"result" falls in the range between 180 and 220.
         return "C";
     }
-    else if(result>150 && result<180){
+    else if(result>150 && result<180){//"result" falls in the range between 150 and 180.
         return "D";
     }
 }
-console.log(creditScore);
+console.log(creditScore);// "log" the "creditScore" function to the "console"
+// However, to actually call the function and see its result, you should use parentheses like this: "console.log(creditScore());"
