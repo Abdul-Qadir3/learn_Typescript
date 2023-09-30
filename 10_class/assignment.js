40f1ffc0d8c7d8474544e3ddf55ab3dd3a97ea7f
Abdul Qadir <aq452831@gmail.com>
2023-09-30 15:01:29 +0500

    class 11

--- 10_class/assignment.js
+++ 10_class/assignment.js
@@ -1,61 +1,60 @@
 "use strict";
-// Sample array to work with
-const myArray = [1, 2, 3, 4, 5];
-// pop: Removes the last element from an array and returns it
-const poppedElement = myArray.pop();
-console.log("Popped element:", poppedElement);
-console.log("Updated array after pop:", myArray);
-// push: Adds one or more elements to the end of an array and returns the new length
-const newLength = myArray.push(6, 7);
-console.log("New array length after push:", newLength);
-console.log("Updated array after push:", myArray);
-// shift: Removes the first element from an array and returns it
-const shiftedElement = myArray.shift();
-console.log("Shifted element:", shiftedElement);
-console.log("Updated array after shift:", myArray);
-// unshift: Adds one or more elements to the beginning of an array and returns the new length
-const newLengthAfterUnshift = myArray.unshift(0, -1);
-console.log("New array length after unshift:", newLengthAfterUnshift);
-console.log("Updated array after unshift:", myArray);
-// concat: Combines two or more arrays and returns a new array
-const anotherArray = [8, 9, 10];
-const combinedArray = myArray.concat(anotherArray);
-console.log("Combined array:", combinedArray);
-// join: Joins all elements of an array into a string, optionally with a specified separator
-const arrayAsString = myArray.join(", ");
-console.log("Array as string:", arrayAsString);
-// slice: Extracts a portion of an array into a new array
-const slicedArray = myArray.slice(2, 4);
-console.log("Sliced array:", slicedArray);
-// indexOf: Returns the first index at which a specified element is found in an array, or -1 if not found
-const indexToFind = 3;
-const indexFound = myArray.indexOf(indexToFind);
-console.log(`Index of ${indexToFind} in the array:`, indexFound);
-// includes: Checks if an array includes a certain element and returns true or false
-const includesElement = myArray.includes(5);
-console.log("Array includes 5:", includesElement);
-// find: Returns the first element in an array that satisfies a provided testing function
-const foundElement = myArray.find((element) => element > 3);
-console.log("First element greater than 3:", foundElement);
-// findIndex: Returns the index of the first element in an array that satisfies a provided testing function
-const foundIndex = myArray.findIndex((element) => element > 3);
-console.log("Index of first element greater than 3:", foundIndex);
-// map: Creates a new array with the results of applying a provided function to every element in the array
-const mappedArray = myArray.map((element) => element * 2);
-console.log("Mapped array (doubled values):", mappedArray);
-// filter: Creates a new array with all elements that pass a provided testing function
-const filteredArray = myArray.filter((element) => element % 2 === 0);
-console.log("Filtered array (even numbers):", filteredArray);
-// reduce: Applies a function to an accumulator and each element in the array (from left to right) to reduce it to a single value
-const sumOfArray = myArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log("Sum of array elements:", sumOfArray);
-// every: Checks if all elements in an array pass a provided testing function
-const allGreaterThanZero = myArray.every((element) => element > 0);
-console.log("All elements greater than 0:", allGreaterThanZero);
-// reverse: Reverses the order of elements in an array in place
-myArray.reverse();
-console.log("Reversed array:", myArray);
-// at: Returns the element at a specified index in an array
-const indexToAccess = 2;
-const elementAtIndex = myArray[indexToAccess];
-console.log(`Element at index ${indexToAccess}:`, elementAtIndex);
+// Sample array for demonstration
+const numbers = [1, 2, 3, 4, 5];
+// pop(): Removes and returns the last element of an array
+const poppedElement = numbers.pop();
+console.log('Popped Element:', poppedElement);
+console.log('Updated Array after pop:', numbers);
+// push(item1, item2, ...): Adds one or more elements to the end of an array
+numbers.push(6, 7);
+console.log('Updated Array after push:', numbers);
+// shift(): Removes and returns the first element of an array
+const shiftedElement = numbers.shift();
+console.log('Shifted Element:', shiftedElement);
+console.log('Updated Array after shift:', numbers);
+// unshift(item1, item2, ...): Adds one or more elements to the beginning of an array
+numbers.unshift(-1, 0);
+console.log('Updated Array after unshift:', numbers);
+// concat(array2, array3, ...): Combines two or more arrays
+const moreNumbers = [8, 9, 10];
+const combinedArray = numbers.concat(moreNumbers);
+console.log('Combined Array:', combinedArray);
+// join(separator): Joins all elements of an array into a string with a specified separator
+const joinedString = numbers.join('-');
+console.log('Joined String:', joinedString);
+// slice(start, end): Returns a new array containing elements from the specified start index to the end index (end not included)
+const slicedArray = numbers.slice(2, 4);
+console.log('Sliced Array:', slicedArray);
+// indexOf(item): Returns the first index at which a given element can be found in the array, or -1 if it is not present
+const indexOfThree = numbers.indexOf(3);
+console.log('Index of 3:', indexOfThree);
+// includes(item): Checks if an element exists in the array and returns a boolean
+const includesFive = numbers.includes(5);
+console.log('Includes 5:', includesFive);
+// find(callback): Returns the first element in the array that satisfies the provided testing function
+const foundElement = numbers.find((num) => num > 3);
+console.log('Found Element:', foundElement);
+// findIndex(callback): Returns the index of the first element in the array that satisfies the provided testing function
+const foundIndex = numbers.findIndex((num) => num > 3);
+console.log('Found Index:', foundIndex);
+// map(callback): Creates a new array with the results of applying a provided function to each element of the original array
+const doubledArray = numbers.map((num) => num * 2);
+console.log('Doubled Array:', doubledArray);
+// filter(callback): Creates a new array with all elements that pass the provided testing function
+const filteredArray = numbers.filter((num) => num > 2);
+console.log('Filtered Array:', filteredArray);
+// reduce(callback, initialValue): Applies a function against an accumulator and each element in the array (left-to-right) to reduce it to a single value
+const sumOfArray = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log('Sum of Array:', sumOfArray);
+// every(callback): Checks if all elements in the array pass the provided testing function and returns a boolean
+const allGreaterThanZero = numbers.every((num) => num > 0);
+console.log('All Greater Than 0:', allGreaterThanZero);
+// sum(): Calculates the sum of all elements in the array
+const arraySum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log('Array Sum:', arraySum);
+// reverse(): Reverses the order of elements in the array
+const reversedArray = [...numbers].reverse();
+console.log('Reversed Array:', reversedArray);
+// at(index): Accesses the element at a specific index in the array
+const elementAtIndex = numbers[2];
+console.log('Element at Index 2:', elementAtIndex);
--- 10_class/assignment.ts
+++ 10_class/assignment.ts
@@ -1,77 +1,77 @@
-// Sample array to work with
-const myArray: number[] = [1, 2, 3, 4, 5];
+// Sample array for demonstration
+const numbers: number[] = [1, 2, 3, 4, 5];
 
-// pop: Removes the last element from an array and returns it
-const poppedElement: number | undefined = myArray.pop();
-console.log("Popped element:", poppedElement);
-console.log("Updated array after pop:", myArray);
+// pop(): Removes and returns the last element of an array
+const poppedElement: number | undefined = numbers.pop();
+console.log('Popped Element:', poppedElement);
+console.log('Updated Array after pop:', numbers);
 
-// push: Adds one or more elements to the end of an array and returns the new length
-const newLength: number = myArray.push(6, 7);
-console.log("New array length after push:", newLength);
-console.log("Updated array after push:", myArray);
+// push(item1, item2, ...): Adds one or more elements to the end of an array
+numbers.push(6, 7);
+console.log('Updated Array after push:', numbers);
 
-// shift: Removes the first element from an array and returns it
-const shiftedElement: number | undefined = myArray.shift();
-console.log("Shifted element:", shiftedElement);
-console.log("Updated array after shift:", myArray);
+// shift(): Removes and returns the first element of an array
+const shiftedElement: number | undefined = numbers.shift();
+console.log('Shifted Element:', shiftedElement);
+console.log('Updated Array after shift:', numbers);
 
-// unshift: Adds one or more elements to the beginning of an array and returns the new length
-const newLengthAfterUnshift: number = myArray.unshift(0, -1);
-console.log("New array length after unshift:", newLengthAfterUnshift);
-console.log("Updated array after unshift:", myArray);
+// unshift(item1, item2, ...): Adds one or more elements to the beginning of an array
+numbers.unshift(-1, 0);
+console.log('Updated Array after unshift:', numbers);
 
-// concat: Combines two or more arrays and returns a new array
-const anotherArray: number[] = [8, 9, 10];
-const combinedArray: number[] = myArray.concat(anotherArray);
-console.log("Combined array:", combinedArray);
+// concat(array2, array3, ...): Combines two or more arrays
+const moreNumbers: number[] = [8, 9, 10];
+const combinedArray: number[] = numbers.concat(moreNumbers);
+console.log('Combined Array:', combinedArray);
 
-// join: Joins all elements of an array into a string, optionally with a specified separator
-const arrayAsString: string = myArray.join(", ");
-console.log("Array as string:", arrayAsString);
+// join(separator): Joins all elements of an array into a string with a specified separator
+const joinedString: string = numbers.join('-');
+console.log('Joined String:', joinedString);
 
-// slice: Extracts a portion of an array into a new array
-const slicedArray: number[] = myArray.slice(2, 4);
-console.log("Sliced array:", slicedArray);
+// slice(start, end): Returns a new array containing elements from the specified start index to the end index (end not included)
+const slicedArray: number[] = numbers.slice(2, 4);
+console.log('Sliced Array:', slicedArray);
 
-// indexOf: Returns the first index at which a specified element is found in an array, or -1 if not found
-const indexToFind: number = 3;
-const indexFound: number = myArray.indexOf(indexToFind);
-console.log(`Index of ${indexToFind} in the array:`, indexFound);
+// indexOf(item): Returns the first index at which a given element can be found in the array, or -1 if it is not present
+const indexOfThree: number = numbers.indexOf(3);
+console.log('Index of 3:', indexOfThree);
 
-// includes: Checks if an array includes a certain element and returns true or false
-const includesElement: boolean = myArray.includes(5);
-console.log("Array includes 5:", includesElement);
+// includes(item): Checks if an element exists in the array and returns a boolean
+const includesFive: boolean = numbers.includes(5);
+console.log('Includes 5:', includesFive);
 
-// find: Returns the first element in an array that satisfies a provided testing function
-const foundElement: number | undefined = myArray.find((element) => element > 3);
-console.log("First element greater than 3:", foundElement);
+// find(callback): Returns the first element in the array that satisfies the provided testing function
+const foundElement: number | undefined = numbers.find((num) => num > 3);
+console.log('Found Element:', foundElement);
 
-// findIndex: Returns the index of the first element in an array that satisfies a provided testing function
-const foundIndex: number = myArray.findIndex((element) => element > 3);
-console.log("Index of first element greater than 3:", foundIndex);
+// findIndex(callback): Returns the index of the first element in the array that satisfies the provided testing function
+const foundIndex: number = numbers.findIndex((num) => num > 3);
+console.log('Found Index:', foundIndex);
 
-// map: Creates a new array with the results of applying a provided function to every element in the array
-const mappedArray: number[] = myArray.map((element) => element * 2);
-console.log("Mapped array (doubled values):", mappedArray);
+// map(callback): Creates a new array with the results of applying a provided function to each element of the original array
+const doubledArray: number[] = numbers.map((num) => num * 2);
+console.log('Doubled Array:', doubledArray);
 
-// filter: Creates a new array with all elements that pass a provided testing function
-const filteredArray: number[] = myArray.filter((element) => element % 2 === 0);
-console.log("Filtered array (even numbers):", filteredArray);
+// filter(callback): Creates a new array with all elements that pass the provided testing function
+const filteredArray: number[] = numbers.filter((num) => num > 2);
+console.log('Filtered Array:', filteredArray);
 
-// reduce: Applies a function to an accumulator and each element in the array (from left to right) to reduce it to a single value
-const sumOfArray: number = myArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log("Sum of array elements:", sumOfArray);
+// reduce(callback, initialValue): Applies a function against an accumulator and each element in the array (left-to-right) to reduce it to a single value
+const sumOfArray: number = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log('Sum of Array:', sumOfArray);
 
-// every: Checks if all elements in an array pass a provided testing function
-const allGreaterThanZero: boolean = myArray.every((element) => element > 0);
-console.log("All elements greater than 0:", allGreaterThanZero);
+// every(callback): Checks if all elements in the array pass the provided testing function and returns a boolean
+const allGreaterThanZero: boolean = numbers.every((num) => num > 0);
+console.log('All Greater Than 0:', allGreaterThanZero);
 
-// reverse: Reverses the order of elements in an array in place
-myArray.reverse();
-console.log("Reversed array:", myArray);
+// sum(): Calculates the sum of all elements in the array
+const arraySum: number = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log('Array Sum:', arraySum);
 
-// at: Returns the element at a specified index in an array
-const indexToAccess: number = 2;
-const elementAtIndex: number | undefined = myArray[indexToAccess];
-console.log(`Element at index ${indexToAccess}:`, elementAtIndex);
+// reverse(): Reverses the order of elements in the array
+const reversedArray: number[] = [...numbers].reverse();
+console.log('Reversed Array:', reversedArray);
+
+// at(index): Accesses the element at a specific index in the array
+const elementAtIndex: number | undefined = numbers[2];
+console.log('Element at Index 2:', elementAtIndex);
