c20e78c3685e5b350862dd2a828b06b7dfed8a23
Abdul Qadir <aq452831@gmail.com>
2023-10-05 10:55:49 +0500

    new addition

--- 02_class/comp_logi_op.ts
+++ 02_class/comp_logi_op.ts
@@ -1,6 +1,6 @@
-// Assignment Operators
+// Comparison operator
 n =5;
-console.log(n==5);//Equals True
+console.log(n==5);//is n is Equals to 5= True
 console.log(n===5);//type checking and value checking
 console.log(n!=5);// not equal to False
 console.log(n>8);// greater then False
@@ -8,11 +8,14 @@ console.log(n<8);// less then True
 console.log(n>=8);// greater the or equal to False
 console.log(n==8);//less than or equal to True
 
-//Logical Operator && ||
+//Logical Operator &&=AND ||=OR
+//AND=True if all conditions are true
+//OR=True if one condition is true
 n=5;
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(n>=5 && n<10);//All condtions must be true to get TRUE
+console.log(n>=5 || n<10);//any condtion true result will be true
+console.log(n>5 && n <10);//false
+console.log(n>=5 || n<10);//true
+console.log(n>5 || n<10);//true
+console.log(!(n<10));//false
+console.log(!(n>10));//True
