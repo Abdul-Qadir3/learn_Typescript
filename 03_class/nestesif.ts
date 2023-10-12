c344b2d18c647f604ad5fd8cfc98180e2c69125f
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2023-10-11 23:27:11 -0700

    nested ifelse statment

--- 03_class/nestesif.ts
+++ 03_class/nestesif.ts
@@ -2,24 +2,23 @@
 let country = ["Pakistan", "Turkey", "Canada"]
 
 // Number() used to convert the string into number
-let age = Math.round(Math.random()*90);
+let age = Math.round(Math.random()*90);// gnerates value bt 0 to 1
 let domicile =["sindh","balochistan","kpk","punjab"];
 
 // let index = Math.random()*10  // num>0 && num<3
 // nested if is used if a condition is bound with another condition and have relation
 if (country[0] === "Pakistan"){
     if(age>=18){
-        console.log("here is your ticket")
+        console.log("here is your ticket for pakistani citizen")
     }else {
-        console.error("Age restriction")
+        console.error("Age restriction for pakistani citizen")
     };
 }else if (country[0]== "Turkey"){
     if (age>=30){
-
-    }else
-    {
-
+        console.log("here is your ticket for turkey citizens")
+    }else{
+        console.error("Age restriction for turkey citizens")
     };
-}else{
+}else{//executes when all above condition are false
     console.log("invalid country");
 };
